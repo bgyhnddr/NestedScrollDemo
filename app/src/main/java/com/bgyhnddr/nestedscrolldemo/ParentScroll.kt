2f37fc2b1781b1cefc6373c872308913bd1cd9e1
beiguanyi <beiguanyi@ut.cn>
2021-08-09 08:55:42 +0800

    Signed-off-by: beiguanyi <beiguanyi@ut.cn>

--- app/src/main/java/com/bgyhnddr/nestedscrolldemo/ParentScroll.kt
+++ app/src/main/java/com/bgyhnddr/nestedscrolldemo/ParentScroll.kt
@@ -10,12 +10,12 @@ import androidx.core.widget.NestedScrollView
 class ParentScroll @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : NestedScrollView(context, attrs, defStyleAttr) {
-
+    private lateinit var topView: View;
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val height = MeasureSpec.getSize(heightMeasureSpec);
         val view = getChildAt(0);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val topView = findViewById<View>(R.id.topBar);
+        topView = findViewById<View>(R.id.topBar);
         view.measure(
             widthMeasureSpec,
             MeasureSpec.makeMeasureSpec(
@@ -26,6 +26,17 @@ class ParentScroll @JvmOverloads constructor(
     }
 
     override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
-        super.onNestedPreScroll(target, dx, dy, consumed, type)
+        Log.v("parent onNestedPreScroll", scrollY.toString())
+        Log.v("parent onNestedPreScroll topView.height", topView.height.toString())
+        Log.v("parent onNestedPreScroll dy", dy.toString())
+        if (dy > 0) {
+            if (scrollY < topView.height) {
+                val leftY = topView.height - scrollY
+                val consumedY = if (dy <= leftY) dy else leftY
+
+                scrollBy(0, consumedY)
+                consumed[1] = consumedY
+            }
+        }
     }
 }
